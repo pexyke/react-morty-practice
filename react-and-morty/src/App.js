04da1846e02090402866cfa0734ba667d857540a
Peter Peko <pekopeter0@gmail.com>
2022-01-27 10:57:08 +0100

    Update App.js

--- react-and-morty/src/App.js
+++ react-and-morty/src/App.js
@@ -78,14 +78,6 @@ function App() {
        }
       }
     
-
-      const onScroll = () => {
-        const scrollY = window.scrollY //Don't get confused by what's scrolling - It's not the window
-        const scrollTop = this.myRef.current.scrollTop
-        console.log(`onScroll, window.scrollY: ${scrollY} myRef.scrollTop: ${scrollTop}`)
-        
-      }
-
   
   return (
   <div className="App" onScroll={handleScroll}>
